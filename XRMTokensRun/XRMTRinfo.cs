28699ce47e130819c2c0b010ff703897effd4ca5
Jonas Rapp <jonas@jonasr.app>
2021-07-13 22:04:55 +0200

    Add my tool name

--- XRMTokensRun/XRMTRinfo.cs
+++ XRMTokensRun/XRMTRinfo.cs
@@ -12,15 +12,13 @@ namespace XRMTokensRun
     // Do not forget to update version number and author (company attribute) in AssemblyInfo.cs class
     // To generate Base64 string for Images below, you can use https://www.base64-image.de/
     [Export(typeof(IXrmToolBoxPlugin)),
-        ExportMetadata("Name", "My First Plugin"),
+        ExportMetadata("Name", "XRM Tokens Runners"),
         ExportMetadata("Description", "This is a description for my first plugin"),
-        // Please specify the base64 content of a 32x32 pixels image
         ExportMetadata("SmallImageBase64", null),
-        // Please specify the base64 content of a 80x80 pixels image
         ExportMetadata("BigImageBase64", null),
-        ExportMetadata("BackgroundColor", "Lavender"),
-        ExportMetadata("PrimaryFontColor", "Black"),
-        ExportMetadata("SecondaryFontColor", "Gray")]
+        ExportMetadata("BackgroundColor", "#FFFFC0"),
+        ExportMetadata("PrimaryFontColor", "#0000C0"),
+        ExportMetadata("SecondaryFontColor", "#0000FF")]
     public class XRMTRinfo : PluginBase
     {
         public override IXrmToolBoxPluginControl GetControl()
