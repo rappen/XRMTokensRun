88145303e451603ad4ff18baec9cec174b571b05
Jonas Rapp <jonas@jonasr.app>
2022-07-23 13:37:48 +0200

    Added Random dialog

---
+++ XRMTokensRun/GetRandom.Designer.cs
@@ -0,0 +1,220 @@
+﻿
+namespace XRMTokensRun
+{
+    partial class GetRandom
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GetRandom));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.lblParam1 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cmbOperator = new System.Windows.Forms.ComboBox();
+            this.txtParam1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.txtParam2 = new System.Windows.Forms.TextBox();
+            this.lblParam2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button1.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.button1.Location = new System.Drawing.Point(138, 237);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(115, 28);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "OK";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button2.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button2.Location = new System.Drawing.Point(272, 237);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(115, 28);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Cancel";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // lblParam1
+            // 
+            this.lblParam1.AutoSize = true;
+            this.lblParam1.Location = new System.Drawing.Point(33, 121);
+            this.lblParam1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblParam1.Name = "lblParam1";
+            this.lblParam1.Size = new System.Drawing.Size(57, 16);
+            this.lblParam1.TabIndex = 11;
+            this.lblParam1.Text = "Param 1";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(138, 46);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(145, 16);
+            this.linkLabel1.TabIndex = 24;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Documentation Column";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(134, 22);
+            this.label10.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(76, 20);
+            this.label10.TabIndex = 23;
+            this.label10.Text = "Random";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(33, 92);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 16);
+            this.label3.TabIndex = 26;
+            this.label3.Text = "Type";
+            // 
+            // cmbOperator
+            // 
+            this.cmbOperator.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbOperator.FormattingEnabled = true;
+            this.cmbOperator.Items.AddRange(new object[] {
+            "Text",
+            "Number",
+            "Date",
+            "Guid"});
+            this.cmbOperator.Location = new System.Drawing.Point(138, 88);
+            this.cmbOperator.Name = "cmbOperator";
+            this.cmbOperator.Size = new System.Drawing.Size(346, 24);
+            this.cmbOperator.TabIndex = 29;
+            this.cmbOperator.SelectedIndexChanged += new System.EventHandler(this.cmbOperator_SelectedIndexChanged);
+            // 
+            // txtParam1
+            // 
+            this.txtParam1.Enabled = false;
+            this.txtParam1.Location = new System.Drawing.Point(138, 118);
+            this.txtParam1.Name = "txtParam1";
+            this.txtParam1.Size = new System.Drawing.Size(346, 22);
+            this.txtParam1.TabIndex = 30;
+            this.txtParam1.TextChanged += new System.EventHandler(this.ShowResult);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 207);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 16);
+            this.label1.TabIndex = 32;
+            this.label1.Text = "Result";
+            // 
+            // txtResult
+            // 
+            this.txtResult.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.txtResult.Location = new System.Drawing.Point(138, 204);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ReadOnly = true;
+            this.txtResult.Size = new System.Drawing.Size(346, 22);
+            this.txtResult.TabIndex = 33;
+            // 
+            // txtParam2
+            // 
+            this.txtParam2.Enabled = false;
+            this.txtParam2.Location = new System.Drawing.Point(138, 146);
+            this.txtParam2.Name = "txtParam2";
+            this.txtParam2.Size = new System.Drawing.Size(346, 22);
+            this.txtParam2.TabIndex = 35;
+            this.txtParam2.TextChanged += new System.EventHandler(this.ShowResult);
+            // 
+            // lblParam2
+            // 
+            this.lblParam2.AutoSize = true;
+            this.lblParam2.Location = new System.Drawing.Point(33, 149);
+            this.lblParam2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblParam2.Name = "lblParam2";
+            this.lblParam2.Size = new System.Drawing.Size(57, 16);
+            this.lblParam2.TabIndex = 34;
+            this.lblParam2.Text = "Param 2";
+            // 
+            // GetRandom
+            // 
+            this.AcceptButton = this.button1;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(575, 276);
+            this.Controls.Add(this.txtParam2);
+            this.Controls.Add(this.lblParam2);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtParam1);
+            this.Controls.Add(this.cmbOperator);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.lblParam1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "GetRandom";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Get Random";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label lblParam1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cmbOperator;
+        private System.Windows.Forms.TextBox txtParam1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.TextBox txtParam2;
+        private System.Windows.Forms.Label lblParam2;
+    }
+}
---
+++ XRMTokensRun/GetRandom.cs
@@ -0,0 +1,82 @@
+﻿using Rappen.XRM.Helpers.Extensions;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace XRMTokensRun
+{
+    public partial class GetRandom : Form
+    {
+        private XRMTR xrmtr;
+
+        public GetRandom()
+        {
+            InitializeComponent();
+        }
+
+        public static string ShowDialog(XRMTR owner)
+        {
+            var dialog = new GetRandom();
+            dialog.xrmtr = owner;
+            if (dialog.ShowDialog((Control)owner) == DialogResult.OK)
+            {
+                return dialog.GetResult();
+            }
+            return null;
+        }
+
+        private string GetResult()
+        {
+            var result = "<random|" +
+                 cmbOperator.Text;
+            if (!string.IsNullOrEmpty(txtParam1.Text))
+            {
+                result += "|" + txtParam1.Text;
+            }
+            if (!string.IsNullOrEmpty(txtParam2.Text))
+            {
+                result += "|" + txtParam2.Text;
+            }
+            result += ">";
+            return result;
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("https://jonasr.app/xrm-tokens/#random");
+        }
+
+        private void cmbOperator_SelectedIndexChanged(object sender, System.EventArgs e)
+        {
+            switch (cmbOperator.Text.ToLowerInvariant())
+            {
+                case "text":
+                    lblParam1.Text = "Length *";
+                    lblParam2.Text = "Characters";
+                    break;
+
+                case "number":
+                    lblParam1.Text = "First number";
+                    lblParam2.Text = "Last number";
+                    break;
+
+                case "date":
+                    lblParam1.Text = "First date";
+                    lblParam2.Text = "Last date";
+                    break;
+
+                default:
+                    lblParam1.Text = "";
+                    lblParam2.Text = "";
+                    break;
+            }
+            txtParam1.Enabled = cmbOperator.Text.ToLowerInvariant() != "guid";
+            txtParam2.Enabled = cmbOperator.Text.ToLowerInvariant() != "guid";
+            ShowResult();
+        }
+
+        private void ShowResult(object sender = null, System.EventArgs e = null)
+        {
+            txtResult.Text = GetResult();
+        }
+    }
+}
